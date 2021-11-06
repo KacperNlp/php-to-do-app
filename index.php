fd4bd0177d188b9b35b72c7060d9d6b15183e508
Kacper Nalepa <kacper.nlp@wp.pl>
2021-11-06 12:21:17 +0100

    Created function for debugging

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
 
-$exampleMessage = 'Hello Man';
+declare(strict_types = 1);
 
-echo "<p>" . $exampleMessage . "</p>";
+function dump($data): void
+{
+    echo '<div 
+    style="
+        display: inline-block;
+        background: #dedede;
+        padding: 10px 30px;
+        border: 1px solid #000;
+    "><prev>';
+    print_r($data);
+    echo '</prev>
+    </div>';
+};
