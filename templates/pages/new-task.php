6a2dc5cba0fb3bb5552e4e04a12b693471c0d3fc
Kacper Nalepa <kacper.nlp@wp.pl>
2021-11-28 19:45:13 +0100

    Added styles for page with new added task container

--- templates/pages/new-task.php
+++ templates/pages/new-task.php
@@ -1,12 +1,12 @@
-<div>
-    <?php if(isset($params['task-title'])): ?>
-        <h2><?= $params['task-title'] ;?></h2>
+<div class="new-task-container">
+    <?php if(isset($params['task-title']) && $params['task-title'] !== '' ): ?>
+        <h2 class="new-task-title"><?= $params['task-title'] ;?></h2>
     <?php else: ?>
-        <p>Your task didn't has title!</p>
+        <p class="empty-form-data">Your task didn't has title!</p>
     <?php endif;?>
-    <?php if(isset($params['description'])):?>
-        <p><?= $params['description'] ;?></p>
+    <?php if(isset($params['description']) && $params['description'] !== '' ):?>
+        <p class="new-task-description"><?= $params['description'] ;?></p>
     <?php else: ?>
-        <p>You didn't set any description for your task...</p>
+        <p class="empty-form-data">You didn't set any description for your task...</p>
     <?php endif;?>
 </div>
